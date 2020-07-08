b5e468ad7a0ddc0bda1bd9cf3ea00634d0b18a59
tmako123 <tomioka_makoto@yahoo.co.jp>
2020-07-08 18:25:12 +0900

    add realtime slam

--- django/catchup/webgl/urls.py
+++ django/catchup/webgl/urls.py
@@ -3,4 +3,6 @@ from . import views
  
 urlpatterns = [
     path('', views.index, name='index'),
+    path('slam', views.slam, name='index'),
+    path('postPose', views.postPose, name='index'),
 ]
--- django/catchup/webgl/views.py
+++ django/catchup/webgl/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render_to_response
+from django.http.response import JsonResponse
+
+import zmq
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.bind("tcp://*:5555")
 
 # Create your views here.
 from django.shortcuts import render
@@ -10,3 +16,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'ply.html')
+    
+def slam(request):
+	return render(request, 'slam.html')
+    
+def postPose(request):
+    socket.send_string("ack")
+
+    message = socket.recv_string()
+    data = [float(x.strip()) for x in message.split(',') if not x.strip() == '']
+    dict = {'frame':data[0], 'x':data[1], 'y':data[2], 'z':data[3], 'ax':data[4], 'ay':data[5], 'az':data[6], 'angle':data[7]}
+    
+    return JsonResponse(dict)
